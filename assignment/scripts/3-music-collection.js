228ad41865c287db9f7c45ce2e756030b014b0db
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-13 14:25:10 -0600

    added collection object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+let album1 ={
+    'title': 'Purple Rain',
+    'artist': 'Prince',
+    'yearPublished': 1984
+}
+
+collection.push(album1);
+console.log(collection);
+
+
+
+
