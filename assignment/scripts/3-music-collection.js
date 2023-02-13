d1926d32e07454aa62397f5cf3feb89b8a5b54df
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-13 14:29:57 -0600

    added one idea for addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,5 +12,25 @@ collection.push(album1);
 console.log(collection);
 
 
+function addToCollection(title, artist, yearPublished){
+    this.title = title;
+    this.artist = artist;
+    this.yearPublished = yearPublished;
+    if(addToCollection === title, artist, yearPublished){
+        collection.push(title, artist, yearPublished);
+    }else{
+        console.log('Not Vaild Entry');
+    }
+}
+
+
+let album = new addToCollection('Coconut Oil', 'Lizzo', 2016);
+console.log(album);
+//console.log(collection.push(album));
+
+let album2 = new addToCollection('Blood on the Tracks', 'Bob Dylam', 1975);
+console.log(album2);
+//console.log(collection.push(album2));
+
 
 
