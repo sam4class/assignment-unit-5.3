6684ee0698579367e9e99449c78158023d9e81e8
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-13 20:17:45 -0600

    edited addToCollecetion function and made showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,7 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+let getAlbumInfo;
 
 let album1 ={
     'title': 'Purple Rain',
@@ -16,13 +17,8 @@ function addToCollection(title, artist, yearPublished){
     this.title = title;
     this.artist = artist;
     this.yearPublished = yearPublished;
-    if(addToCollection === title, artist, yearPublished){
-        collection.push(title, artist, yearPublished);
-    }else{
-        console.log('Not Vaild Entry');
-    }
+       collection.push({title, artist, yearPublished});
 }
-
 
 let album = new addToCollection('Coconut Oil', 'Lizzo', 2016);
 console.log(album);
@@ -32,17 +28,61 @@ let album2 = new addToCollection('Blood on the Tracks', 'Bob Dylam', 1975);
 console.log(album2);
 //console.log(collection.push(album2));
 
-let album3 = new addToCollection('HighLife', 1999);
+let album3 = new addToCollection('The Jets', 'The Jets', 1985);
 console.log(album3);
-
-let album4 = new addToCollection('The Jets', 'The Jets', 1985);
+let album4 = new addToCollection('Fontanelle', 'Babes In Toyland', 1992);
 console.log(album4);
-let album5 = new addToCollection('Fontanelle', 'Babes In Toyland', 1992);
+let album5 = new addToCollection('Ipecac Neat', 'P.O.S', 2004);
 console.log(album5);
-let album6 = new addToCollection('Ipecac Neat', 'P.O.S', 2004);
+let album6 = new addToCollection('Sing the Dancing 20s', 'The Andrews Sisiters', 1958);
 console.log(album6);
-let album7 = new addToCollection('Sing the Dancing 20s', 'The Andrews Sisiters', 1958);
-console.log(album7);
 
 //After all are added, console.log collection array
 console.log(collection);
+
+//Adding showCollection function
+
+function showCollection(array){
+    console.log(array.length);
+    for(let i=0; i<array.length; i++){
+       let arr = array[i].title;
+       let arr1 = array[i].artist;
+       let arr2 = array[i].yearPublished;
+    console.log(arr + ' by ' + arr1 + ' published in ' + arr2);
+    }
+}
+console.log(showCollection(collection));
+
+//for (let variable in object) {
+    // code to be executed
+  //}
+
+//function showCollection(array){
+  //  console.log(array.length);
+    //array.forEach(addToCollection);
+    //console.log(title + ' by ' + artist + ' published in '+yearPublished);
+//}
+
+//function showCollection(array){
+  //  console.log(array.length);
+    //let index = 0;
+	//while (index<array.length){
+    //console.log(array[title] + ' by ' + array[artist] + ' published in '+array[yearPublished]);
+    //console.log(title + ' by ' + artist + ' published in '+yearPublished);
+    //index++
+ //}
+//}
+
+//array.forEach(addToCollectoin(array)){
+  //  console.log(title + ' by ' + artist + ' published in '+yearPublished);
+//}
+
+//function showCollection(array){
+  //  console.log(array.length);
+    //for(i=0; i<array.length; i++){
+      //  array.forEach(addToCollection);
+        //console.log(title + ' by ' + artist + ' published in '+yearPublished);
+    //}
+//}
+//console.log(showCollection(collection));
+//array.forEach(addToCollection);
